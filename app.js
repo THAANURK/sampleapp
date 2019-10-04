237de27f14f165c5980df78077fb1fcdebce30ed
shreethaanu raveendran <shree96thaanu@gmail.com>
2019-10-04 11:32:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/health', (req, res) => res.send('Hello thaanu!'))
+app.get('/health', (req, res) => res.send('Hello kanmaniii!'))
 app.use('/',express.static('public'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
