e3f513908db2ec57121785cd38f2860375f4bb70
shreethaanu raveendran <shree96thaanu@gmail.com>
2019-10-04 11:09:15 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello thaanu!'))
+app.get('/health', (req, res) => res.send('Hello thaanu!'))
+app.use('/',express.static('public'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
